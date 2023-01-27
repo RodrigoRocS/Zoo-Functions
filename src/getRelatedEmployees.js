0c3ac99ebe68cc8753fb4e8fcc77683a89a63bb0
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-27 20:34:36 -0300

    requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,12 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees.some((e) => e.managers.some((el) => el === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const pegaManager = data.employees.filter((e) => e.managers.find((el) => el === managerId));
+    return pegaManager.map((e) => `${e.firstName} ${e.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
