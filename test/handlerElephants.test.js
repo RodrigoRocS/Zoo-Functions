4e133da50ae376c39e927d5398d0ca33f86544ba
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-27 22:00:37 -0300

    requisito 7 concluido

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -22,6 +22,12 @@ describe('Testes da função HandlerElephants', () => {
   it('Verifica se "handlerElephants" retorna undefined se não passado nenhum argumento', () => {
     expect(handlerElephants()).toBe(undefined);
   });
+  it('Verifica se "handlerElephants" retorna "Parâmetro inválido, é necessário uma string" se o argumento não for uma string', () => {
+    expect(handlerElephants(5)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica se "handlerElephants" retorna null se o parâmetro não for válido', () => {
+    expect(handlerElephants('bebe')).toBe(null);
+  });
   it('Verifica se "handlerElephants" retorna [ "Friday", "Saturday", "Sunday", "Tuesday"] com o argumento "availability" ', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
   });
