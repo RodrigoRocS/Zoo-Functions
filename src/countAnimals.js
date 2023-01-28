7a15c2a7a7de19834b35f74afcd7d9184c0c8b56
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-27 21:19:22 -0300

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const animais = data.species.find((e) => e.name === animal.species);
+  if (Object.keys(animal).includes('sex')) {
+    return animais.residents.filter((el) => el.sex === animal.sex).length;
+  }
+  return animais.residents.length;
 };
+
+// const res = data.species.map((e) => e.residents);
 
 module.exports = countAnimals;
