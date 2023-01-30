8636163ebd2828636e7079ec3be519f11c4d3a54
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-30 11:24:08 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const pegaId = data.employees.find((e) => e.id === id);
+  const pegaPrimeiro = pegaId.responsibleFor[0];
+  const pegaOldest = data.species.find((e) => e.id === pegaPrimeiro).residents.sort((a, b) => a.age - b.age);
+  return Object.values(pegaOldest[pegaOldest.length - 1]);
 };
 
 module.exports = getOldestFromFirstSpecies;
